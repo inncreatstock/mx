eb60265119b877a983414b6c6432d40a096381dc
inncreatstock <89166958+inncreatstock@users.noreply.github.com>
2021-10-08 14:28:50 -0500

    Add files via upload

---
+++ profiles/inncreat/script_buttons.js
@@ -0,0 +1,9 @@
+var follow;
+
+follow = document.getElementById("follow");
+follow.href = "https://www.facebook.com/Inncreat-110905050773165";
+
+var discover;
+
+discover = document.getElementById("discover");
+discover.href = "https://www.youtube.com/channel/UCqNAPfHmIMJqDgJ7bb0_hyA";
---
+++ profiles/inncreat/script_profile.js
@@ -0,0 +1,14 @@
+var perfil;
+
+perfil = document.getElementById("perfil");
+perfil.innerHTML = "&nbsp;Inncreat inc. /";
+
+var rango;
+
+rango = document.getElementById("rango");
+rango.innerHTML = "Colaborador";
+
+var web;
+
+web = document.getElementById("web");
+web.href = "https://www.inncreat.ga";
