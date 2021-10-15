b3d85820b7071995ecf6a28547403aab45539ce7
inncreatstock <89166958+inncreatstock@users.noreply.github.com>
2021-10-15 13:30:38 -0500

    Add files via upload

---
+++ gallery/announced/script_global.js
@@ -0,0 +1,9 @@
+var announcement1;
+
+	announcement1 = document.getElementById("announcement1");
+	announcement1.href="";
+
+var announcement1_0;
+
+	announcement1_0 = document.getElementById("announcement1_0");
+	announcement1_0.src="https://www.inncreat-stock.ga/gallery/advertisements/global.png";
---
+++ gallery/partial/announced/script_partial.js
@@ -0,0 +1,19 @@
+var announcement2;
+
+	announcement2 = document.getElementById("announcement2");
+	announcement2.href="hola1";
+
+var announcement2_1;
+
+	announcement2_1 = document.getElementById("announcement2_1");
+	announcement2_1.src="https://www.inncreat-stock.ga/gallery/partial/advertisements/partial.png";
+
+var announcement3;
+
+	announcement3 = document.getElementById("announcement3");
+	announcement3.href="hola11";
+
+var announcement3_2;
+
+	announcement3_2 = document.getElementById("announcement3_2");
+	announcement3_2.src="https://www.inncreat-stock.ga/gallery/partial/advertisements/partial.png";
